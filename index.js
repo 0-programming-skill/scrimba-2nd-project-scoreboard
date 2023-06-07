20ee7edc0f8c97d5f054196aed7fe840626f63e1
0-programming-skill <still.standing@tutanota.com>
2023-06-06 22:24:57 -0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeScore = 0;
+let guestScore = 0;
+const homeScoreElement = document.getElementById('js-home-score');
+const guestScoreElement = document.getElementById('js-guest-score');
+
+function updateScore(){
+    homeScoreElement.textContent = homeScore;
+    guestScoreElement.textContent = guestScore;
+}
+
+updateScore();
+
+function increaseScore(who, amount) {
+    
+    if (who === 'home' && amount === 1) {
+        homeScore += 1;
+        
+    } else if (who === 'home' && amount === 2) {
+        homeScore += 2;
+        
+    } else if (who === 'home' && amount === 3) {
+        homeScore += 3;
+        
+    } else if (who === 'guest' && amount === 1) {
+        guestScore += 1;
+        
+    } else if (who === 'guest' && amount === 2) {
+        guestScore += 2;
+        
+    } else if (who === 'guest' && amount === 3) {
+        guestScore += 3;
+    }
+    
+    updateScore();
+}
