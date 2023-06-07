a6d2e04b354812def879def3c81e9d860d0c5306
0-programming-skill <79772980+0-programming-skill@users.noreply.github.com>
2023-06-06 22:48:48 -0300

    size fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let guestScore = 0;
 const homeScoreElement = document.getElementById('js-home-score');
 const guestScoreElement = document.getElementById('js-guest-score');
 
-function updateScore(){
+function updateScore() {
     homeScoreElement.textContent = homeScore;
     guestScoreElement.textContent = guestScore;
 }
@@ -11,25 +11,25 @@ function updateScore(){
 updateScore();
 
 function increaseScore(who, amount) {
-    
+
     if (who === 'home' && amount === 1) {
         homeScore += 1;
-        
+
     } else if (who === 'home' && amount === 2) {
         homeScore += 2;
-        
+
     } else if (who === 'home' && amount === 3) {
         homeScore += 3;
-        
+
     } else if (who === 'guest' && amount === 1) {
         guestScore += 1;
-        
+
     } else if (who === 'guest' && amount === 2) {
         guestScore += 2;
-        
+
     } else if (who === 'guest' && amount === 3) {
         guestScore += 3;
     }
-    
+
     updateScore();
 }
